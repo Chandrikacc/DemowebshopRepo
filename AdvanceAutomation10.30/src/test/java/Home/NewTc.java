20b1057dff7e45e5e367d8f4011406216016e71e
Mamatha K R <mamatharangaswamaiah@gmail.com>
2023-11-07 12:46:46 +0530

    This is a commit from TE

---
+++ AdvanceAutomation10.30/src/test/java/Home/NewTc.java
@@ -0,0 +1,12 @@
+package Home;
+
+import org.testng.annotations.Test;
+
+import genericUtilities.BaseClass;
+
+public class NewTc extends BaseClass {
+	@Test
+	public void demo() {
+		driver.navigate().to("https://www.facebook.com");
+	}
+}
